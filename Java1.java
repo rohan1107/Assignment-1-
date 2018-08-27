d2be3da75f0740b230436c8491c2acfc8e09b905
rohan1107 <42728578+rohan1107@users.noreply.github.com>
2018-08-27 09:43:43 +0530

    Add files via upload

---
+++ Java1.java
@@ -0,0 +1,18 @@
+public class Java1
+{
+public static void main(String[] x)
+{
+System.out.println("hello world");
+
+}
+
+
+}
+
+public class Exercise1 {
+ 
+ public static void main(String[] args) {
+  System.out.println("Hello\n Rohan Dhiman!");
+ }
+ 
+}
